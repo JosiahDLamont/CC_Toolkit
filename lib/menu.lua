4f74fa0630afe30f3e579f86c88a65475d4e70ba
Josiah Lamont <47308442+JosiahDLamont@users.noreply.github.com>
2022-08-04 08:19:56 -0400

    Update menu.lua

--- lib/menu.lua
+++ lib/menu.lua
@@ -1,49 +1,57 @@
+local Menu = {}
 
-local buttons = {}
-local marker_index = 1
+--builds a menu
+function new_menu()
+    menu = {}
+    menu.buttons = {}
+    menu.marker_index = 1
+
+    setmetatable(menu, Menu)
+    return menu
+end
 
 --adds a new button to buttons
-local function new_button(display_name, callback)
+function Menu.new_button(self, display_name, callback)
     b = { text=display_name, func=callback }
-    table.insert(buttons, b)
+    table.insert(self.buttons, b)
 end
 
 -- this function activates the button located at index, if it exists and has a function
-local function select_button(index)
-    if buttons[index] == nil then
+function Menu.select_button(self, index)
+    if self.buttons[index] == nil then
         return
-    elseif buttons[index].func == nil then
+    elseif self.buttons[index].func == nil then
         return
     else
-        buttons[index].func()
+        self.buttons[index].func()
     end
 end
 
 --parses key input to navigate the menu
-local function parse_key()
+function Menu.parse_key(self)
 
-    num_buttons = table.getn(buttons)
+    num_buttons = table.getn(self.buttons)
     local event, key = os.pullEvent("key")
     if key == keys.up then
-        marker_index = marker_index - 1
+        self.marker_index = self.marker_index - 1
     elseif key == keys.down then
-        marker_index = marker_index + 1
+        self.marker_index = self.marker_index + 1
     elseif key == keys.enter then
-        select_button(marker_index)
+        self:select_button(self.marker_index)
     end
 
-    if marker_index < 1 then
-        marker_index = marker_index + num_buttons
-    elseif marker_index > num_buttons then
-        marker_index = marker_index - num_buttons
+    if self.marker_index < 1 then
+        self.marker_index = self.marker_index + num_buttons
+    elseif self.marker_index > num_buttons then
+        self.marker_index = self.marker_index - num_buttons
     end
 
 end
 
 --prints the buttons to the terminal without clearing it first
-local function print_buttons()
+function Menu.print_buttons(self)
     local marker = "ERR"
-    for num, button in pairs(buttons) do
+    for num, button in pairs(self.buttons) do
 
         if marker_index == num then
             marker = "["..num.."]"
@@ -55,37 +63,4 @@ local function print_buttons()
     end
 end
 
---clears the terminal
-local function clear()
-    term.clear()
-    term.setCursorPos(1,1)
-end
-
---placeholder callback function for testing
-local function foobar()
-    clear()
-    print("Success!")
-    os.sleep(.5)
-end
-
---driver function to test functionality
-local function driver()
-    meta_commands = { should_exit=false, banner = "#### Banner ####" }
-    new_button("hi", foobar)
-    new_button("hey", function() meta_commands.banner = "???? Banner ????" end)
-    new_button("foo", function() meta_commands.banner = "<><> Banner <><>" end)
-    new_button("exit", function() meta_commands.should_exit = true end)
-
-    while not meta_commands.should_exit do
-
-        clear()
-        print(meta_commands.banner)
-        print_buttons()
-        parse_key()
-    end
-    clear()
-end
-
-
-driver()
-print("Program Terminated Successfully!")
+return Menu
